208390ab09166e8aa3b43b969ffffed6099e2f75
ni8mr <nilpaddo@gmail.com>
2018-01-11 17:51:31 +0600

    Add coreui

---
+++ README.md
@@ -0,0 +1,5 @@
+### COREUI with django ###
+
+
+
+
---
+++ project_config/urls.py
@@ -0,0 +1,29 @@
+"""coreui_boilerplate URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', home)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
